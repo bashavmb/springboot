d6d86d39c13585ebffede06e49a9135382f8d8d8
bashavmb <basha.vmb@gmail.com>
2020-08-27 23:26:15 -0400

    initial commit

---
+++ gradlecopy.sh
@@ -0,0 +1,4 @@
+cp -r microservices/product-service/gradle .; \
+cp microservices/product-service/gradlew .; \
+cp microservices/product-service/gradlew.bat .; \
+cp microservices/product-service/.gitignore .
